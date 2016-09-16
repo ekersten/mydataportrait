0e317944a959b11364aaa006f1ec6ab95b121796
Eric Kersten <eric.kersten@wunderman.com>
2016-09-16 15:54:39 -0300

    avances templates y cambios de modelos

--- dataportrait/booth/models.py
+++ dataportrait/booth/models.py
@@ -4,11 +4,14 @@ from django.conf import settings
 
 
 class Photo(models.Model):
-    code = models.CharField(max_length=30)
-    image = models.ImageField(upload_to='uploads/')
+    code = models.CharField(max_length=4)
+    image = models.ImageField(upload_to='uploads/', blank=True)
 
     def image_tag(self):
-        return u'<img src="{0}{1}" width="150">'.format(settings.MEDIA_URL, self.image)
+        if self.image:
+            return u'<img src="{0}{1}" width="150">'.format(settings.MEDIA_URL, self.image)
+        else:
+            return u''
 
     def delete(self, *args, **kwargs):
         self.image.delete()
--- dataportrait/booth/views.py
+++ dataportrait/booth/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
 from django.template.context import Context
 
+from .models import Photo
+
 
 def index(request):
     context = Context({})
     return render(request, 'index.html', context)
+
+
+def picture(request):
+    context = Context({})
+
+    code = request.POST.get('code', None)
+
+    if code is not None:
+         try:
+             photo = Photo.objects.get(code=code)
+             context['photo'] = photo
+             return render(request, 'photo.html', context)
+         except Photo.DoesNotExist:
+             context['error'] = True
+             return render(request, 'photo.html', context)
