ad7c5c76cd053c2c46b74560d6fad13c1c7dc22b
Eric Kersten <eric.kersten@wunderman.com>
2016-09-20 18:03:54 -0300

    photo generator template and view

--- dataportrait/booth/urls.py
+++ dataportrait/booth/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^photo/$', views.picture, name='picture'),
+    url(r'^photo/(?P<code>[-\w\d]+)/(?P<network>[facebook|linkedin]+)$', views.picture_generator, name='picture_generator'),
 ]
--- dataportrait/booth/views.py
+++ dataportrait/booth/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.urls import resolve
 from django.template.context import Context
+from django.http import HttpResponse
 
 from .models import Photo
 
@@ -23,3 +26,22 @@ def picture(request):
             context['error'] = True
 
         return render(request, 'photo.html', context)
+
+
+def picture_generator(request, code, network):
+    context = Context({})
+
+    if not request.user.is_authenticated():
+        return redirect(resolve('booth:index'))
+
+    if code and network:
+        code = str(code).lower()
+        if code is not None:
+            try:
+                photo = Photo.objects.get(code=code)
+                context['photo'] = photo
+                context['network'] = network
+            except Photo.DoesNotExist:
+                context['error'] = True
+
+            return render(request, 'photo_generator.html', context)
