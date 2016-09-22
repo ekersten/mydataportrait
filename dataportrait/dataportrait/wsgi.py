f955c01e0a17827de95f544750409fbfc1b9b7ed
Eric Kersten <eric.kersten@wunderman.com>
2016-09-22 16:47:58 -0300

    changed wsgi file

--- dataportrait/dataportrait/wsgi.py
+++ dataportrait/dataportrait/wsgi.py
@@ -7,7 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
+
+sys.path.append('/home/development/projects/dataprtrait/dataportrait')
+
+sys.path.appen('/home/development/.virtualenvs/dataportrait/lib/python3.5/site-packages')
 
 from django.core.wsgi import get_wsgi_application
 
