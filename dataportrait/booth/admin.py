328418f90bd2d65f9b5055ee61d42be5ca1888ed
Eric Kersten <eric.kersten@wunderman.com>
2016-09-22 14:51:22 -0300

    admin serach by code

--- dataportrait/booth/admin.py
+++ dataportrait/booth/admin.py
@@ -5,6 +5,7 @@ from .models import Photo
 
 class PhotoAdmin(admin.ModelAdmin):
     list_display = ('code', 'image_tag')
+    search_fields = ['code',]
     exclude = ('image_tag',)
 
 admin.site.register(Photo, PhotoAdmin)
