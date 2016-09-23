a9b051e432701d0d570e913d2c10ea64fa176f9a
Eric Kersten <eric.kersten@wunderman.com>
2016-09-23 14:28:59 +0000

    basic deploy script

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+echo "Updating from Git"
+git pull
+echo "Activating virtualenv"
+source /home/development/.virtualenvs/dataportrait/bin/activate
+echo "Installing requirements"
+cd /home/development/projects/dataportrait/dataportrait
+pip install -r requirements.txt
+echo "Updating DB Schema"
+python manage.py migrate
+echo "Collecting static assets"
+python manage.py collectstatic --no-input --clear
+echo "Deactivating virtualenv"
+deactivate
+echo "Deploy complete!"
