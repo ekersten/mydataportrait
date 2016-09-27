3859a6a2c5b2c5fa999da8f8d0bc673749832cb2
Eric Kersten <eric.kersten@wunderman.com>
2016-09-27 12:31:37 -0300

    cambios esteticos para pantallas chicas

--- dataportrait/booth/static/js/photo.js
+++ dataportrait/booth/static/js/photo.js
@@ -1,6 +1,7 @@
 (function($) {
     $('.yes_no.yes').on('click', function() {
         $('.yes_no_actions').hide();
+        $('.isthisyou').hide();
         $('.social_login').show().removeClass('hidden');
     });
 })(jQuery);
